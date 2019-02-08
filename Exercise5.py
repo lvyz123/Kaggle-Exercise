d1e0c9a063218d348904c1d0b705ba10c80c9c38
lvyz123 <35937287+lvyz123@users.noreply.github.com>
2019-02-08 00:24:31 -0500

    Update Exercise5.py

--- Exercise5.py
+++ Exercise5.py
@@ -33,3 +33,24 @@ from learntools.core import binder
 binder.bind(globals())
 from learntools.machine_learning.ex5 import *
 print("\nSetup complete")
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+
+candidate_max_leaf_nodes = [5, 25, 50, 100, 250, 500]
+# Write loop to find the ideal tree size from candidate_max_leaf_nodes
+min_mae_value=0
+for max_leaf_node in candidate_max_leaf_nodes:
+    mae_value=get_mae(max_leaf_node, train_X, val_X, train_y, val_y)
+    if min_mae_value==0 or min_mae_value>mae_value:
+        min_mae_value=mae_value
+        min_mae_index=candidate_max_leaf_nodes.index(max_leaf_node)
+
+# Store the best value of max_leaf_nodes (it will be either 5, 25, 50, 100, 250 or 500)
+best_tree_size = candidate_max_leaf_nodes[min_mae_index]
+
+step_1.check()
