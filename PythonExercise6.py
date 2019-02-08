29ef3b196f48744dd135e8ebe8a317e71507a8d0
lvyz123 <35937287+lvyz123@users.noreply.github.com>
2019-02-08 00:56:43 -0500

    Create PythonExercise6.py

---
+++ PythonExercise6.py
@@ -0,0 +1,191 @@
+# SETUP. You don't need to worry for now about what this code does or how it works. If you're ever curious about the 
+# code behind these exercises, it's available under an open source license here: https://github.com/Kaggle/learntools/
+from learntools.core import binder; binder.bind(globals())
+from learntools.python.ex7 import *
+print('Setup complete.')
+
+# Import the jimmy_slots submodule
+from learntools.python import jimmy_slots
+# Call the get_graph() function to get Jimmy's graph
+graph = jimmy_slots.get_graph()
+graph
+
+def prettify_graph(graph):
+    """Modify the given graph according to Jimmy's requests: add a title, make the y-axis
+    start at 0, label the y-axis. (And, if you're feeling ambitious, format the tick marks
+    as dollar amounts using the "$" symbol.)
+    """
+    graph.set_title("Results of 500 slot machine pulls")
+    graph.set_ylim(bottom=0)
+    graph.set_ylabel("Balance")
+
+graph = jimmy_slots.get_graph()
+prettify_graph(graph)
+graph
+
+[
+    {'name': 'Peach', 'items': ['green shell', 'banana', 'green shell',], 'finish': 3},
+    {'name': 'Bowser', 'items': ['green shell',], 'finish': 1},
+    # Sometimes the racer's name wasn't recorded
+    {'name': None, 'items': ['mushroom',], 'finish': 2},
+    {'name': 'Toad', 'items': ['green shell', 'mushroom'], 'finish': 1},
+]
+
+def best_items(racers):
+    """Given a list of racer dictionaries, return a dictionary mapping items to the number
+    of times those items were picked up by racers who finished in first place.
+    """
+    winner_item_counts = {}
+    for i in range(len(racers)):
+        # The i'th racer dictionary
+        racer = racers[i]
+        # We're only interested in racers who finished in first
+        if racer['finish'] == 1:
+            for i in racer['items']:
+                # Add one to the count for this item (adding it to the dict if necessary)
+                if i not in winner_item_counts:
+                    winner_item_counts[i] = 0
+                winner_item_counts[i] += 1
+
+        # Data quality issues :/ Print a warning about racers with no name set. We'll take care of it later.
+        if racer['name'] is None:
+            print("WARNING: Encountered racer with unknown name on iteration {}/{} (racer = {})".format(
+                i+1, len(racers), racer['name'])
+                 )
+    return winner_item_counts
+
+sample = [
+    {'name': 'Peach', 'items': ['green shell', 'banana', 'green shell',], 'finish': 3},
+    {'name': 'Bowser', 'items': ['green shell',], 'finish': 1},
+    {'name': None, 'items': ['mushroom',], 'finish': 2},
+    {'name': 'Toad', 'items': ['green shell', 'mushroom'], 'finish': 1},
+]
+best_items(sample)
+
+# Import luigi's full dataset of race data
+from learntools.python.luigi_analysis import full_dataset
+
+# Fix me!
+def best_items(racers):
+    winner_item_counts = {}
+    for i in range(len(racers)):
+        # The i'th racer dictionary
+        racer = racers[i]
+        # We're only interested in racers who finished in first
+        if racer['finish'] == 1:
+            for j in racer['items']:
+                # Add one to the count for this item (adding it to the dict if necessary)
+                if i not in winner_item_counts:
+                    winner_item_counts[j] = 0
+                winner_item_counts[j] += 1
+
+        # Data quality issues :/ Print a warning about racers with no name set. We'll take care of it later.
+        if racer['name'] is None:
+            print("WARNING: Encountered racer with unknown name on iteration {}/{} (racer = {})".format(
+                i+1, len(racers), racer['name'])
+                 )
+    return winner_item_counts
+
+# Try analyzing the imported full dataset
+best_items(full_dataset)
+
+def blackjack_hand_greater_than(hand_1, hand_2):
+    """
+    Return True if hand_1 beats hand_2, and False otherwise.
+    
+    In order for hand_1 to beat hand_2 the following must be true:
+    - The total of hand_1 must not exceed 21
+    - The total of hand_1 must exceed the total of hand_2 OR hand_2's total must exceed 21
+    
+    Hands are represented as a list of cards. Each card is represented by a string.
+    
+    When adding up a hand's total, cards with numbers count for that many points. Face
+    cards ('J', 'Q', and 'K') are worth 10 points. 'A' can count for 1 or 11.
+    
+    When determining a hand's total, you should try to count aces in the way that 
+    maximizes the hand's total without going over 21. e.g. the total of ['A', 'A', '9'] is 21,
+    the total of ['A', 'A', '9', '3'] is 14.
+    
+    Examples:
+    >>> blackjack_hand_greater_than(['K'], ['3', '4'])
+    True
+    >>> blackjack_hand_greater_than(['K'], ['10'])
+    False
+    >>> blackjack_hand_greater_than(['K', 'K', '2'], ['3'])
+    False
+    """
+    value_1=0;value_2=0
+    for card in hand_1:
+        if card.isdigit():
+            value_1+=int(card)
+        elif card in ['J', 'Q', 'K']:
+            value_1+=10
+        elif card=='A':
+            value_1+=1
+    if value_1<=11 and 'A' in hand_1:
+        value_1+=10
+    for card in hand_2:
+        if card.isdigit():
+            value_2+=int(card)
+        elif card in ['J', 'Q', 'K']:
+            value_2+=10
+        elif card=='A':
+            value_2+=1
+    if value_2<=11 and 'A' in hand_2:
+        value_2+=10
+    if (value_2>21 and value_1<=21) or (value_1>value_2 and value_1<=21):
+        return True
+    return False
+
+q3.check()
+
+from learntools.python import roulette
+import random
+
+def random_and_superstitious(wheel):
+    """Interact with the given wheel over 100 spins with the following strategy:
+    - if the wheel lands on 4, don't bet on the next spin
+    - otherwise, bet on a random number on the wheel (from 0 to 10)
+    """
+    last_number = 0
+    while wheel.num_remaining_spins() > 0:
+        if last_number == 4:
+            # Unlucky! Don't bet anything.
+            guess = None
+        else:
+            guess = random.randint(0, 10)
+        last_number = wheel.spin(number_to_bet_on=guess)
+
+roulette.evaluate_roulette_strategy(random_and_superstitious)
+
+def my_agent(wheel):
+    """Interact with the given wheel over 100 spins with the following strategy:
+    - if the wheel lands on 4, don't bet on the next spin
+    - otherwise, bet on a random number on the wheel (from 0 to 10)
+    """
+    last_number = 0
+    num_rec={}
+    import numpy as np
+    while wheel.num_remaining_spins() > 0:
+        if last_number == 4 and '4' not in num_rec.keys():
+            # Unlucky! Don't bet anything.
+            guess = None
+            last_number = wheel.spin(number_to_bet_on=guess)
+            num_rec['4']=[last_number]
+        elif last_number == 4 and num_rec['4']:
+            a = np.array(num_rec['4'])
+            counts = np.bincount(a)
+            guess = np.argmax(counts)
+            last_number = wheel.spin(number_to_bet_on=guess)
+            num_rec['4'].append(last_number)
+        elif str(last_number) not in num_rec.keys():
+            guess = None
+            last_number = wheel.spin(number_to_bet_on=guess)
+            num_rec[int(last_number)]=[last_number]
+        else:
+            dist_mean = np.mean(num_rec[str(last_number)])
+            guess = round(dist_mean)
+            last_number = wheel.spin(number_to_bet_on=guess)
+            num_rec[int(last_number)].append(last_number)
+
+roulette.evaluate_roulette_strategy(my_agent)
